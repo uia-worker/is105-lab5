acd9b1610bddd260fc23ac13892fb07046602922
Janis Gailis <janis.gailis@uia.no>
2021-03-22 16:01:21 +0100

    Første versjon av wordcount

---
+++ wordcount.go
@@ -0,0 +1,44 @@
+package main 
+
+import (
+    "fmt"
+    "io"
+    "log"
+    "os"
+    "unicode"
+    "github.com/pkg/profile"
+)
+
+func readbyte(r io.Reader) (rune, error) {
+    var buf [1]byte
+    _, err := r.Read(buf[:])
+    return rune(buf[0]), err
+}
+
+func main() {
+
+    // For å generere rapporten om profil
+    defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+
+    f, err := os.Open(os.Args[1])
+    if err != nil {
+        log.Fatalf("Kunne ikke åpne filen %q: %v", os.Args[1], err)
+    }
+   
+    words := 0  
+    inword := false  // tilstandsmaskin at vi er inn et ord (obs! unicode) 
+    for {
+        r, err := readbyte(f)
+        if err == io.EOF {
+            break
+        } 
+        if unicode.IsSpace(r) && inword {
+            words++
+            inword = false
+        }
+        inword = unicode.IsLetter(r)
+    } 
+    
+    fmt.Printf("%q: %d words\n", os.Args[1], words)
+
+}
